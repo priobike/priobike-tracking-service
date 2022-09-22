f8099d8d738e64bfa8f1b0ffce2d513176f219c2
Philipp Matthes <philipp.matthes@tu-dresden.de>
2022-09-22 12:52:28 +0200

    Parse query arguments correctly

--- backend/tracks/views.py
+++ backend/tracks/views.py
@@ -78,15 +78,18 @@ class ListTracksResource(View):
         tracks = Track.objects.all()
         
         # Filter the tracks by the requested parameters.
-        if "from" in request.GET:
-            tracks = tracks.filter(start_time__gte=request.GET["from"])
-        if "to" in request.GET:
-            tracks = tracks.filter(end_time__lte=request.GET["to"])
-        if "debug" in request.GET:
-            tracks = tracks.filter(debug=request.GET["debug"])
-        if "backend" in request.GET:
+        if "from" in request.GET: # Start time. (int)
+            tracks = tracks.filter(start_time__gte=int(request.GET["from"]))
+        if "to" in request.GET: # End time. (int)
+            tracks = tracks.filter(end_time__lte=int(request.GET["to"]))
+        if "debug" in request.GET: # Debug mode. (bool)
+            if request.GET["debug"] == "true":
+                tracks = tracks.filter(debug=True)
+            elif request.GET["debug"] == "false":
+                tracks = tracks.filter(debug=False)
+        if "backend" in request.GET: # Backend. (str)
             tracks = tracks.filter(backend=request.GET["backend"])
-        if "positioning" in request.GET:
+        if "positioning" in request.GET: # Positioning mode. (str)
             tracks = tracks.filter(positioning_mode=request.GET["positioning"])
         
         # Paginate the tracks.
