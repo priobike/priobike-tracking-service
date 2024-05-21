9872341d7f432ed9ff40b1dff48bd5aa86427e42
Philipp Matthes <philipp.matthes@tu-dresden.de>
2024-05-21 13:11:22 +0200

    Fix deserialization error

--- backend/tracks/fields.py
+++ backend/tracks/fields.py
@@ -15,16 +15,16 @@ class JSONField(models.TextField):
         # If the value is a single-quote string, replace it with a double-quote string.
         if isinstance(value, str) and "'" in value:
             value = value.replace("'", '"')
-        # Convert uppercase True and False to lowercase.
+        # Convert uppercase True and False to lowercase, and None to null.
         if isinstance(value, str):
             value = value.replace("True", "true").replace("False", "false")
+            value = value.replace("None", "null")
         # ---- End of checks
         try:
             return json.loads(value)
         except (TypeError, ValueError) as e:
             # Print out a detailed error message.
             print(f"Error: {e}")
-            print(f"Value: {value}")
             raise
 
     def from_db_value(self, value, *args):
