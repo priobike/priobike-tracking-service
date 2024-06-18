85c2b73b9e86331aa4cdce1e09570bf926c66d0c
adeveloper-wq <dajesch@gmail.com>
2024-06-18 09:46:20 +0200

    Use json.dump for to-string of JSONField instead of to-string of dict

--- backend/tracks/fields.py
+++ backend/tracks/fields.py
@@ -11,25 +11,14 @@ class JSONField(models.TextField):
     def to_python(self, value):
         if not value: 
             return None
-        print(value)
-        # ---- Checks if the Django encoder was used to serialize the data.
-        # If the value is a single-quote string, replace it with a double-quote string.
-        if isinstance(value, str) and "'" in value:
-            value = value.replace("'", '"')
         # When the string contains symbols like Ã\\x9f (ß), we need to use the correct encoding.
         if isinstance(value, str) and "\\x" in value:
             value = bytes(value, "utf-8").decode("unicode_escape")
-        # Convert uppercase True and False to lowercase, and None to null.
-        if isinstance(value, str):
-            value = value.replace("True", "true").replace("False", "false")
-            value = value.replace("None", "null")
-        # ---- End of checks
         try:
             return json.loads(value)
         except (TypeError, ValueError) as e:
             # Print out a detailed error message.
             print(f"Error: {e}")
-            print(value)
             raise
 
     def from_db_value(self, value, *args):
@@ -40,3 +29,9 @@ class JSONField(models.TextField):
         if isinstance(value, dict):
             value_for_db = json.dumps(value, cls=DjangoJSONEncoder)
         return value_for_db
+    
+    def value_to_string(self, obj):
+        value = self.value_from_object(obj)
+        if value is not None:
+            return json.dumps(value, cls=DjangoJSONEncoder)
+        return ''
